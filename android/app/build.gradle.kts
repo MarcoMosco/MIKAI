f05fdc3dbe4322864101affe458021148b0567bd
Marco Mosco <marco.mosco@gmail.com>
2025-06-27 15:22:23 +0200

    Descrizione delle modifiche

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,17 +7,21 @@ plugins {
 
 android {
     namespace = "com.example.material_orders_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdkVersion(34)
     ndkVersion = "27.0.12077973"
     // ... altre configurazioni ...
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
     }
 
     defaultConfig {
@@ -25,21 +29,24 @@ android {
         applicationId = "com.example.material_orders_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdkVersion(21)
+        targetSdkVersion(34)
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
 }
